3f03e743d80e9d9a9be9a267353ff0f8bf867872
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-07-08 22:45:06 -0300

    refatorando o css que tava mais feio que eu

---
+++ src.js
@@ -0,0 +1,6 @@
+const horas = document.querySelector('#hr');
+const minutos = document.querySelector('#min');
+const segundos = document.querySelector('#seg');
+horas.innerText = 15;
+minutos.innerText = 25;
+segundos.innerText = 31;
