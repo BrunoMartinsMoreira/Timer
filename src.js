a20fb96c70bac12c7848f6e7e31f2665fe41cb5a
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-07-09 22:04:53 -0300

    fazendo algumas mudanças pequenas

--- src.js
+++ src.js
@@ -1,58 +1,61 @@
-const relogio = document.querySelector('.relogio');
-const iniciarBtn = document.querySelector('.iniciar');
-const pauseBtn = document.querySelector('.pausar');
-const zerarBtn = document.querySelector('.zerar');
+function iniciaCronometro() {
+   const relogio = document.querySelector('.relogio');
+   const iniciarBtn = document.querySelector('.iniciar');
+   const pauseBtn = document.querySelector('.pausar');
+   const zerarBtn = document.querySelector('.zerar');
 
-let segundos = 0;
-let minutos = 0;
-let horas = 0;
-let cronometro;
-let tempo = 1000;
+   let segundos = 0;
+   let minutos = 0;
+   let horas = 0;
+   let cronometro;
+   let tempo = 1000;
 
-iniciarBtn.addEventListener('click', start);
-pauseBtn.addEventListener('click', pause);
-zerarBtn.addEventListener('click', zerarTimer);
+   iniciarBtn.addEventListener('click', start);
+   pauseBtn.addEventListener('click', pause);
+   zerarBtn.addEventListener('click', zerarTimer);
 
-function start() {
-   cronometro = setInterval(() => {
-      timer();
-   }, tempo)
-};
+   function start() {
+      cronometro = setInterval(() => {
+         timer();
+      }, tempo)
+   };
 
-function pause() {
-   clearInterval(cronometro);
-};
+   function pause() {
+      clearInterval(cronometro);
+   };
 
-function zerarTimer() {
-   clearInterval(cronometro);
-   segundos = 0;
-   minutos = 0;
-   horas = 0;
+   function zerarTimer() {
+      clearInterval(cronometro);
+      segundos = 0;
+      minutos = 0;
+      horas = 0;
 
-   relogio.innerHTML = `
+      relogio.innerHTML = `
    <span>00</span>:
    <span>00</span>:
    <span>00</span>`
-};
-
-function timer() {
-   segundos++;
-   if (segundos == 59) {
-      segundos = 0
-      minutos++
    };
 
-   if (minutos == 59) {
-      minutos = 0
-      horas++
-   };
+   function timer() {
+      segundos++;
+      if (segundos == 59) {
+         segundos = 0
+         minutos++
+      };
 
-   let formatSegundos = segundos < 10 ? `0${segundos}` : segundos;
-   let formatMinutos = minutos < 10 ? `0${minutos}` : minutos;
-   let formatHoras = horas < 10 ? `0${horas}` : horas;
+      if (minutos == 59) {
+         minutos = 0
+         horas++
+      };
 
-   relogio.innerHTML = `
+      let formatSegundos = segundos < 10 ? `0${segundos}` : segundos;
+      let formatMinutos = minutos < 10 ? `0${minutos}` : minutos;
+      let formatHoras = horas < 10 ? `0${horas}` : horas;
+
+      relogio.innerHTML = `
     <span>${formatHoras}</span>:
    <span>${formatMinutos}</span>:
    <span>${formatSegundos}</span>`
+   }
 }
+iniciaCronometro();
